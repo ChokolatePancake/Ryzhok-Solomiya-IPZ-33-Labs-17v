eb8c3a23ab022a9b3b71de0bc35fae5e10491cfa
Solya <catsubrosa@gmail.com>
2024-10-17 09:25:48 +0300

    Lab3

--- lib/lab3/lab3.dart
+++ lib/lab3/lab3.dart
@@ -1,14 +1,15 @@
-import 'package:flutter/tabulation.dart';
+import 'tabulation.dart';
 
 void main() {
   final tabulator = Tabulation(step: 0.1);
 
-  tabulator.setRangeFrom(-2.0);
+  // tabulator.setRangeFrom(-2.0);
   tabulator.setRangeTo(3.0);
 
   tabulator.run();
 
-  print("Середнє арифметичне значення функції: ${tabulator.getAverage()}");
-  print("Мінімальне значення функції: ${tabulator.getMin()}");
-  print("Максимальне значення функції: ${tabulator.getMax()}");
+  print("Average: ${tabulator.getAverage()}");
+  print("Min: ${tabulator.getMin()}");
+  print("Max: ${tabulator.getMax()}");
+  print("Sum: ${tabulator.getSum()}");
 }
