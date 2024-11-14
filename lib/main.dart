cca7c734e231044a0c312dc579573e92fc23cc51
Solya <catsubrosa@gmail.com>
2024-11-14 05:04:30 +0200

    Lab 6

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import './widgets/sign_in_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lab-6',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: false,
+      ),
+      home: const SignInScreen(),
+    );
+  }
+}
